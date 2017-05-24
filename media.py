c9700c6649faf9b3d6f9faf7662105dcdff72996
kpl00 <joshuaylw@gmail.com>
2017-05-24 23:41:40 +0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,10 @@
+# A class that incorporates attributes of particular movies
+class Movie():
+
+    def __init__(self, genre, title, year, director, youtube_trailer, box_art):
+        self.genre = genre
+        self.title = title
+        self.year = year
+        self.director = director
+        self.youtube_trailer = youtube_trailer
+        self.box_art = box_art
